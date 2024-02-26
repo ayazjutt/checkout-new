0435d98c17ca530b2f79fc2a23205ef2921f19ba
aaqibsheikh <aaqibnadeem123@gmail.com>
2024-02-26 18:33:39 +0500

    thank you page fixes

--- resources/views/partials/steps/step-5.blade.php
+++ resources/views/partials/steps/step-5.blade.php
@@ -2,15 +2,15 @@
 
     <div class="flex h-screen items-center justify-center bg-gray-100 py-10">
         <div class="mx-auto max-w-md overflow-hidden rounded-3xl text-gray-700 shadow-md">
-            <div class="h-40 bg-rose-500 pt-10 sm:h-56">
+            <div class="h-40 bg-[#8E5D0B] pt-10 sm:h-56">
                 <img class="h-full w-full object-contain" src="{{asset('assets/images/thank-you-bizvee.png')}}" alt="" />
             </div>
             <div class="flex flex-col items-center bg-white px-4 py-10">
-                <h2 class="mb-2 text-3xl font-bold text-rose-500 sm:text-4xl">Thank you!</h2>
+                <h2 class="mb-2 text-3xl font-bold text-[#8E5D0B] sm:text-4xl">Thank you!</h2>
                 <p class="mb-8 font-medium text-gray-500">Our support team will contact you with in 24 hours. </p>
 {{--                <div>--}}
 
-{{--                    <a href="#" class="inline-flex items-center justify-center p-5 text-base font-medium text-white rounded-lg bg-rose-500 hover:text-white hover:bg-rose-400 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">--}}
+{{--                    <a href="#" class="inline-flex items-center justify-center p-5 text-base font-medium text-white rounded-lg bg-[#8E5D0B] hover:text-white hover:bg-rose-400 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">--}}
 {{--                        <span class="w-full">Get started with our portal to track your orders</span>--}}
 {{--                        <svg class="w-4 h-4 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">--}}
 {{--                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>--}}
@@ -18,7 +18,7 @@
 {{--                    </a>--}}
 
 {{--                </div>--}}
-                <div class="flex items-center rounded-xl bg-rose-500 p-4 cursor-pointer">
+                <div class="flex items-center rounded-xl bg-[#8E5D0B] p-4 cursor-pointer">
                     <div class="ml-4 w-56 text-white font-bold">
                         Click here to track your order
                     </div>
