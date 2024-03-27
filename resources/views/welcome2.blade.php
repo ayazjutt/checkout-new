536543fbf92b192bbaaae9153b6e99efd19cbe47
Ayaz Sadiq <ayaz.sadiq01@gmail.com>
2024-03-28 00:14:41 +0500

    fixes

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -66,144 +66,147 @@
 
 </head>
 
-<body class="antialiased relative bg-[#f5f8fa]">
-@if(!empty($country))
-    <script>
-        var additionalServicesData = <?php echo json_encode($country->additional_services); ?>;
-    </script>
-@endif
+<body class="antialiased relative">
+<div class="bg-[#f5f8fa] h-full">
+    @if(!empty($country))
+        <script>
+            var additionalServicesData = <?php echo json_encode($country->additional_services); ?>;
+        </script>
+    @endif
 
-<div class="fixed top-0 left-0 w-full h-full bg-white opacity-75 z-50 overflow-hidden pointer-events-none" id="loader" style="display: none">
-    <div class="flex justify-center items-center h-full">
-        <div class="animate-spin text-violet-600 fas fa-circle-notch fa-5x"></div>
+    <div class="fixed top-0 left-0 w-full h-full bg-white opacity-75 z-50 overflow-hidden pointer-events-none" id="loader" style="display: none">
+        <div class="flex justify-center items-center h-full">
+            <div class="animate-spin text-violet-600 fas fa-circle-notch fa-5x"></div>
+        </div>
     </div>
+
+    <form class="lg:h-full relative lg:pb-0" action="{{ route('checkout') }}" method="post" id="registerForm" name="registerForm">
+
+        @csrf
+        <!-- Mobile Header -->
+        <div class="grid grid-cols-10 h-full lg:mt-0 mt-16" id="thank_you_page" style="display: none;">
+            @include('partials.steps.step-5')
+        </div>
+
+        <div  id="form_area"
+              class="bg-cover bg-no-repeat bg-center h-full bg-[#f5f8fa]">
+            <div class="flex items-center justify-center pt-8 w-full lg:hidden">
+                <img src='assets/images/logo.png' alt="Logo" class="w-20" />
+            </div>
+
+            <div class="flex items-center justify-center mt-5 mb-8 lg:hidden">
+                <nav aria-label="Progress">
+                    <ol role="list" class="flex items-center justify-center">
+                        <li class="relative pr-14 sm:pr-20">
+                            <!-- Completed Step -->
+                            <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                                <div class="h-0.5 w-full bg-[#DA0000]"></div>
+                            </div>
+                            <a href="#" data-step="1" id="progress-step-1-mobile" class="relative flex h-8 w-8 items-center justify-center rounded-full bg-[#DA0000]">
+                                <span class="text-white">1</span>
+                                <span class="sr-only">Step 1</span>
+                                <span class="absolute top-8 text-[#191919]">Type</span>
+                            </a>
+
+                        </li>
+                        <li class="relative pr-14 sm:pr-20">
+                            <!-- Completed Step -->
+                            <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                                <div class="h-0.5 w-full bg-[#D3BEA9]"></div>
+                            </div>
+                            <a href="#" data-step="2" id="progress-step-2-mobile"
+                               class="relative flex h-8 w-8 items-center justify-center rounded-full bg-[#D3BEA9] border border-[#C3B09B]">
+                                <span class="text-[#C3B09B]">2</span>
+
+                                <span class="sr-only">Step 2</span>
+                                <span class="absolute top-8 text-[#13131380]">Details</span>
+                            </a>
+                        </li>
+                        <li class="relative pr-14 sm:pr-20">
+                            <!-- Current Step -->
+                            <div class="absolute inset-0 flex items-center" aria-hidden="true">
+                                <div class="h-0.5 w-full bg-[#D3BEA9]"></div>
+                            </div>
+                            <a href="#" data-step="3" id="progress-step-3-mobile"
+                               class="relative flex h-8 w-8 items-center justify-center rounded-full bg-[#D3BEA9] border border-[#C3B09B]"
+                               aria-current="step">
+                                <span class="text-[#C3B09B]">3</span>
+                                <span class="sr-only">Step 3</span>
+                                <span class="absolute top-8 text-[#13131380]">Preview</span>
+
+                            </a>
+                        </li>
+                        <li class="relative">
+                            <a href="#" data-step="4" id="progress-step-4-mobile"
+                               class="group relative flex h-8 w-8 items-center justify-center rounded-full bg-[#D3BEA9] border border-[#C3B09B]">
+                                <span class="text-[#C3B09B]">4</span>
+
+                                <span class="sr-only">Step 4</span>
+                                <span class="absolute top-8 text-[#13131380]">Payment</span>
+
+                            </a>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+
+            <div class="grid grid-cols-10 h-full lg:mt-0 mt-16">
+                {{--  Left Side Bar  --}}
+                @include('partials.left-side-bar')
+
+                {{--  Form Area  --}}
+                <div class="lg:col-span-6 col-span-12 lg:py-8 pb-0 lg:px-20 px-4 bg-cover bg-center text-white h-auto"
+                >
+                    {{--  Step 1  --}}
+                    @include('partials.steps.step-1')
+
+                    {{--  Step 2  --}}
+                    @include('partials.steps.step-2')
+
+                    {{--  Step 3  --}}
+                    @include('partials.steps.step-3')
+
+                    {{--  Step 4  --}}
+                    @include('partials.steps.step-4')
+
+                </div>
+
+                {{--  Right Side Bar  --}}
+                @include('partials.right-side-bar')
+            </div>
+            <div class="lg:hidden block sticky bottom-0 z-10 h-36 bg-white text-black w-full rounded-tl-2xl rounded-tr-2xl" id="bottomDiv">
+                <div class="w-full flex items-center justify-center mobile-summary">
+                    <div class="h-1 bg-[#DA0000] w-40 my-2 rounded"></div>
+                </div>
+
+                <div class="px-4 w-full">
+                    <div class="flex items-start justify-between my-4 mobile-summary">
+                        <p class="ml-4 font-semibold text-lg text-[#292929]">Total payment</p>
+                        <div class="flex flex-col space-y-2">
+                            <p class="font-bold text-3xl text-[#DA0000]">
+                                $<span class="total_pay_amount_summary">{{ $processing_types[0]->amount + $state_amount }}</span>
+                            </p>
+                            <p class="font-normal underline text-xs text-[#292929]">View Price Detail</p>
+                        </div>
+                    </div>
+
+                    <div class="w-full hidden mb-6" id="summary">
+                        @include('partials.summary')
+                    </div>
+
+
+
+                    <button type="button" class="w-full h-8 bg-[#DA0000] text-white text-center font-semibold text-base rounded-lg next mobile-view">Next</button>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </form>
 </div>
 
-<form class="lg:h-full relative lg:pb-0" action="{{ route('checkout') }}" method="post" id="registerForm" name="registerForm">
-
-    @csrf
-    <!-- Mobile Header -->
-    <div class="grid grid-cols-10 h-full lg:mt-0 mt-16" id="thank_you_page" style="display: none;">
-        @include('partials.steps.step-5')
-    </div>
-
-    <div  id="form_area"
-        class="bg-cover bg-no-repeat bg-center h-full bg-[#f5f8fa]">
-        <div class="flex items-center justify-center pt-8 w-full lg:hidden">
-            <img src='assets/images/logo.png' alt="Logo" class="w-20" />
-        </div>
-
-        <div class="flex items-center justify-center mt-5 mb-8 lg:hidden">
-            <nav aria-label="Progress">
-                <ol role="list" class="flex items-center justify-center">
-                    <li class="relative pr-14 sm:pr-20">
-                        <!-- Completed Step -->
-                        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="h-0.5 w-full bg-[#DA0000]"></div>
-                        </div>
-                        <a href="#" data-step="1" id="progress-step-1-mobile" class="relative flex h-8 w-8 items-center justify-center rounded-full bg-[#DA0000]">
-                            <span class="text-white">1</span>
-                            <span class="sr-only">Step 1</span>
-                            <span class="absolute top-8 text-[#191919]">Type</span>
-                        </a>
-
-                    </li>
-                    <li class="relative pr-14 sm:pr-20">
-                        <!-- Completed Step -->
-                        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="h-0.5 w-full bg-[#D3BEA9]"></div>
-                        </div>
-                        <a href="#" data-step="2" id="progress-step-2-mobile"
-                            class="relative flex h-8 w-8 items-center justify-center rounded-full bg-[#D3BEA9] border border-[#C3B09B]">
-                            <span class="text-[#C3B09B]">2</span>
-
-                            <span class="sr-only">Step 2</span>
-                            <span class="absolute top-8 text-[#13131380]">Details</span>
-                        </a>
-                    </li>
-                    <li class="relative pr-14 sm:pr-20">
-                        <!-- Current Step -->
-                        <div class="absolute inset-0 flex items-center" aria-hidden="true">
-                            <div class="h-0.5 w-full bg-[#D3BEA9]"></div>
-                        </div>
-                        <a href="#" data-step="3" id="progress-step-3-mobile"
-                            class="relative flex h-8 w-8 items-center justify-center rounded-full bg-[#D3BEA9] border border-[#C3B09B]"
-                            aria-current="step">
-                            <span class="text-[#C3B09B]">3</span>
-                            <span class="sr-only">Step 3</span>
-                            <span class="absolute top-8 text-[#13131380]">Preview</span>
-
-                        </a>
-                    </li>
-                    <li class="relative">
-                        <a href="#" data-step="4" id="progress-step-4-mobile"
-                            class="group relative flex h-8 w-8 items-center justify-center rounded-full bg-[#D3BEA9] border border-[#C3B09B]">
-                            <span class="text-[#C3B09B]">4</span>
-
-                            <span class="sr-only">Step 4</span>
-                            <span class="absolute top-8 text-[#13131380]">Payment</span>
-
-                        </a>
-                    </li>
-                </ol>
-            </nav>
-        </div>
-
-        <div class="grid grid-cols-10 h-full lg:mt-0 mt-16">
-            {{--  Left Side Bar  --}}
-            @include('partials.left-side-bar')
-
-            {{--  Form Area  --}}
-            <div class="lg:col-span-6 col-span-12 lg:py-8 pb-0 lg:px-20 px-4 bg-cover bg-center text-white h-auto"
-            >
-                {{--  Step 1  --}}
-                @include('partials.steps.step-1')
-
-                {{--  Step 2  --}}
-                @include('partials.steps.step-2')
-
-                {{--  Step 3  --}}
-                @include('partials.steps.step-3')
-
-                {{--  Step 4  --}}
-                @include('partials.steps.step-4')
-
-            </div>
-
-            {{--  Right Side Bar  --}}
-            @include('partials.right-side-bar')
-        </div>
-        <div class="lg:hidden block sticky bottom-0 z-10 h-36 bg-white text-black w-full rounded-tl-2xl rounded-tr-2xl" id="bottomDiv">
-            <div class="w-full flex items-center justify-center mobile-summary">
-                <div class="h-1 bg-[#DA0000] w-40 my-2 rounded"></div>
-            </div>
-    
-            <div class="px-4 w-full">
-                <div class="flex items-start justify-between my-4 mobile-summary">
-                    <p class="ml-4 font-semibold text-lg text-[#292929]">Total payment</p>
-                    <div class="flex flex-col space-y-2">
-                        <p class="font-bold text-3xl text-[#DA0000]">
-                            $<span class="total_pay_amount_summary">{{ $processing_types[0]->amount + $state_amount }}</span>
-                        </p>
-                        <p class="font-normal underline text-xs text-[#292929]">View Price Detail</p>
-                    </div>
-                </div>
-    
-                <div class="w-full hidden mb-6" id="summary">
-                    @include('partials.summary')
-                </div>
-    
-    
-    
-                <button type="button" class="w-full h-8 bg-[#DA0000] text-white text-center font-semibold text-base rounded-lg next mobile-view">Next</button>
-            </div>
-    
-        </div>
-    </div>
-
-
-
-</form>
 </body>
 
 </html>
